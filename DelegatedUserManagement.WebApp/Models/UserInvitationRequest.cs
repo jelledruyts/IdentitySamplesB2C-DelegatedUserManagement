593d9b0f29db111346e8b9196b0eaf0500f8773b
Jelle Druyts <jelle_druyts@yahoo.com>
2020-10-26 14:28:12 +0100

    Sample was moved, replacing local repo with pointer

--- DelegatedUserManagement.WebApp/Models/UserInvitationRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace DelegatedUserManagement.WebApp
-{
-    public class UserInvitationRequest
-    {
-        public string CompanyId { get; set; }
-        public string DelegatedUserManagementRole { get; set; }
-        public int ValidHours { get; set; }
-    }
-}
